639c36bb07d03ca13f43898ccf5a737ec94aaca5
Adjocacher Abednego <abednegoadjocacher@gmail.com>
2024-02-12 12:12:28 -1200

    To calculate the  area and the perimeter of rectangle

---
+++ area_peri.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+/**
+ * To print the area and the perimeter of a rectangle
+*/
+using namespace std;
+int main()
+{
+    int lenght;
+    int breadth;
+    int area;
+    cout<<"enter the lenght: "/*<<endl*/;
+    cin>>lenght;
+    cout<<"enter breadth: "/*<<endl*/;
+    cin>>breadth;
+    area = lenght * breadth;
+    cout<<"The area of the rectangle is "<<area<<endl;
+     int perimeter;
+    perimeter = 2 * (lenght + breadth);
+    cout << "The perimeter of a rectangle is "<< perimeter <<endl;
+    return 0;
+}
