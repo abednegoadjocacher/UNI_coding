fc02c73f40aba9987e9acd064b28f6b872f80c02
Adjocacher Abednego <abednegoadjocacher@gmail.com>
2024-02-27 19:32:28 -1200

    A for loop

---
+++ for.cxx
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //A for loop to print numbers
+    for(int i = 0; i <= 20; i++)
+    {
+    cout << i << endl;
+    }
+    cout<<"The 2nd loop"<<endl;
+    //Other ways to write a for loop
+    int j;
+    for (j=1; j<10; j++)
+    cout << j << endl;
+    return 0;
+}
