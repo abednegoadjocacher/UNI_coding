5c4bb018639a3819dd29e91ac294acefca74001e
Adjocacher Abednego <abednegoadjocacher@gmail.com>
2024-03-05 02:43:06 -1200

    Largest

---
+++ largest.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using  namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"Enter three numbers : ";
+    cin>>a>>b>>c;
+    if (a>b&&a>c&&b)
+    cout<<a<<':'<<"is largest"<<endl;
+    else if(b>a&&b>c)
+    cout<<b<<':'<<"is largest"<<endl;
+
+    else 
+    cout <<c<< ": "<<"largest"<<endl;
+}
+
