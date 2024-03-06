588d4d2f0ab5bcddafd51bbf6f92e20cf80f7f1e
Adjocacher Abednego <abednegoadjocacher@gmail.com>
2024-03-06 05:34:46 -1200

    keep practicing

--- factorial.cpp
+++ factorial.cpp
@@ -5,20 +5,16 @@ int main()
     cout <<"Enter a number : ";
     int num;
     cin >> num;
-    int fact = 1;
-    
-   if (num < 0)
+    int factorial = 1;
+   if (num < 0) // To chcek for a negative input
    cout<<"Can't find the factorial of a  negative number "<<num<<"!"<<endl;
-   else if(num <= 1)
-   cout<<"The factorial of the number  "<<num<<"! "<<"is 1"<<endl;
+   else if(num <= 1) // To check if input is '1' or '0'
+   cout<<"The factorial of the number  "<<num<<"! "<<" is 1"<<endl;
    else
    {
    for(int i=1; i<=num; i++)
-   {
-    fact = fact *  i;
-   }
-    cout<<"The factorial of the number is : "<<fact<<endl;
-   
+    factorial = factorial *  i;
+    cout<<"The factorial of the number is : "<<factorial<<endl;
    }
     return 0;
 }
