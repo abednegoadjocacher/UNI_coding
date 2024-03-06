7dbb346ee33b1f8e2460f3ef9a889f6949a379fd
Adjocacher Abednego <abednegoadjocacher@gmail.com>
2024-03-05 23:14:40 -1200

    Factorial

--- factorial.cpp
+++ factorial.cpp
@@ -5,9 +5,20 @@ int main()
     cout <<"Enter a number : ";
     int num;
     cin >> num;
-    int fact = 0;
-    for(int i = num;i < 0; num++)
-    fact *=(--num);
+    int fact = 1;
+    
+   if (num < 0)
+   cout<<"Can't find the factorial of a  negative number "<<num<<"!"<<endl;
+   else if(num <= 1)
+   cout<<"The factorial of the number  "<<num<<"! "<<"is 1"<<endl;
+   else
+   {
+   for(int i=1; i<=num; i++)
+   {
+    fact = fact *  i;
+   }
     cout<<"The factorial of the number is : "<<fact<<endl;
+   
+   }
     return 0;
 }
