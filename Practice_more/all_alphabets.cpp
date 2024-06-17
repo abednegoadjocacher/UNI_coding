69a546991a43c7e3e3ff3414f52bde3a3d93fc1f
Adjocacher Abednego <141531265+abednegoadjocacher@users.noreply.github.com>
2024-06-17 05:24:46 +0000

    start coding

---
+++ Practice_more/all_alphabets.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+/**
+ * A program to print all the alphabets
+ * I used a for loop.
+*/
+cout<<"\n\n";
+cout<<"Letters in Lower case"<<"\n";
+cout<<" ___________________________________________ "<<endl <<"\n";
+for(char lowercaps = 'a'; lowercaps <= 'z'; lowercaps++)
+{
+//cout<<alpha<<endl;
+cout<<"The ASCII value of the alphabet "<<lowercaps<<" is "<<int(lowercaps)<<"\n";
+}
+cout<<"\n\n";
+cout<<"Letters in Upper case"<<"\n";
+cout<<" ___________________________________________ "<<endl <<"\n";
+for(char uppercaps = 'A'; uppercaps <= 'Z'; uppercaps++)
+cout<<"The ASCII value of upper case letter "<<uppercaps<<" is "<<int(uppercaps)<<"\n";
+return 0;
+}
