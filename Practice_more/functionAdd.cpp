16dae609f892e14d07b6c92008e97a88d58e0f19
Adjocacher Abednego <141531265+abednegoadjocacher@users.noreply.github.com>
2024-09-17 13:54:54 +0000

    more of functions

---
+++ Practice_more/functionAdd.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void add (char);
+int main()
+{ int summation= 0;
+char ch = 'u';
+    cout<<"Hello ";
+  add(ch);
+   // cout<<summation<<endl;
+}
+
+void add(char c)
+{
+    cout<<"World"<<endl;
+}
